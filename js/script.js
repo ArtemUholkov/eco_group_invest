c2adac36bc6070c53e94ac0f23cdd4924b7607df
Artem <artem.uholkov@ukr.net>
2022-11-11 13:09:11 +0200

    thank you page created

--- js/script.js
+++ js/script.js
@@ -41,28 +41,28 @@ function myFunction() {
   }, 2000);
 }
 
-// const faders = document.querySelectorAll('.fade-in');
-// const sliders = document.querySelectorAll('.slide-in');
-// const appearOptions = { threshold: 0, rootMargin: '0px 0px -150px 0px' };
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+const appearOptions = { threshold: 0, rootMargin: '0px 0px -150px 0px' };
 
-// const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     } else {
-//       entry.target.classList.add('appear');
-//       appearOnScroll.unobserve(entry.target);
-//     }
-//   });
-// }, appearOptions);
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
 
-// faders.forEach((fader) => {
-//   appearOnScroll.observe(fader);
-// });
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
 
-// sliders.forEach((slider) => {
-//   appearOnScroll.observe(slider);
-// });
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
 
 const logo = document.querySelector('.header_logo');
 console.log(logo);
